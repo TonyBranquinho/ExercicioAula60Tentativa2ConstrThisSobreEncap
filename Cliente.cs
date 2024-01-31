12ac808674afbf1a3ab09e4a3f0931047dfe9d7c
Tony <antony.branquinho@gmail.com>
2024-01-31 14:34:05 -0300

    bem dificil, nao consegui fazer mais que isso e nao ta funcionando

---
+++ Cliente.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioAula60Tentativa2ConstrThisSobreEncap {
+    internal class Cliente {
+
+        CultureInfo CI = CultureInfo.InvariantCulture;
+
+        public int NumeroConta;
+        public string TitularConta;
+        public double DepositoInicial;
+        public double Saldo;
+
+        public Cliente() { 
+        }
+
+        public Cliente(int numeroConta, string titularConta) {
+            NumeroConta = numeroConta;
+            TitularConta = titularConta;
+        }
+
+        public double Deposito(double deposito) {
+            return deposito + Saldo + DepositoInicial;
+        }
+
+
+        public override string ToString() {
+            return "Conta "
+                + NumeroConta
+                + ", Titular: "
+                + TitularConta
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CI);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using ExercicioAula60Tentativa2ConstrThisSobreEncap;
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            
+            Console.Write("Digite o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Digite o titular da conta: ");
+            string titularConta = Console.ReadLine();
+
+            Cliente cli = new Cliente(numeroConta, titularConta);
+
+            Console.Write("Haverá depósito inicial(s / n) ? ");
+            char teste = char.Parse(Console.ReadLine());
+
+            Cliente clie = new Cliente();
+
+            clie.DepositoInicial = 0;
+
+            if(teste == 's') {
+                Console.Write("Digite o valor de depósito inicial: ");
+                clie.DepositoInicial = double.Parse(Console.ReadLine(), CI);
+            }      
+
+            
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(cli.ToString());
+
+        }
+    }
+}
